d9d995e403489184061b69f9e3d8db4b386f8d1d
Shaun Saini <shaunsaini03@berkeley.edu>
2022-03-27 17:41:37 -0700

    completed

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState, useEffect, useMemo} from "react";
+import Board from "./Board";
 import './styles/Square.css';
 
-function Square() {
+
+
+function Square(props) {
     return (
-        <button className="square">
-            {/* TODO */}
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
     )
 }
